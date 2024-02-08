5b3cecd0ec26c0237fa59c77d2b492e3023d52c9
frontend210 <sr.frontenddev210@gmail.com>
2024-02-07 23:04:47 -0800

    update eslint plugin, rules for react, typescript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,16 @@
 module.exports = {
     extends: [
-        'semistandard',
         'plugin:react/recommended',
         'plugin:react-hooks/recommended',
         'plugin:import/recommended',
+        'standard-with-typescript',
         'prettier',
     ],
     rules: {
         'react/react-in-jsx-scope': 'off',
         'react/jsx-uses-react': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/triple-slash-reference': 'off',
     },
+    ignorePatterns: ['vite.config.ts', 'vite-env.d.ts'],
 }
